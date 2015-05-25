901c7b5480198be1141b9f06d0e133a8f6fb324d
anestos <n_anestos@yahoo.gr>
2015-05-25 11:50:55 +0200

    Added connection between NatTraversalComp and Timer

--- src/main/java/se/kth/swim/HostComp.java
+++ src/main/java/se/kth/swim/HostComp.java
@@ -16,7 +16,6 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
-
 package se.kth.swim;
 
 import java.util.ArrayList;
@@ -42,12 +41,13 @@ import se.sics.p2ptoolbox.util.network.NatedAddress;
  * @author Alex Ormenisan <aaor@sics.se>
  */
 public class HostComp extends ComponentDefinition {
+
     private static final Logger log = LoggerFactory.getLogger(HostComp.class);
     private Positive<Network> network = requires(Network.class);
     private Positive<Timer> timer = requires(Timer.class);
 
     private final NatedAddress selfAddress;
-    
+
     private Component swim;
     private Component nat;
     private Component croupier;
@@ -55,24 +55,25 @@ public class HostComp extends ComponentDefinition {
     public HostComp(HostInit init) {
         this.selfAddress = init.selfAddress;
         log.info("{} initiating...", new Object[]{selfAddress});
-        
+
         subscribe(handleStart, control);
         subscribe(handleStop, control);
-        
+
         int overlayId = 1; //so far we don' start multiple croupier overlay
         croupier = create(CroupierComp.class, new CroupierComp.CroupierInit(selfAddress, new ArrayList<NatedAddress>(init.bootstrapNodes), init.seed, init.croupierConfig, overlayId));
         connect(croupier.getNegative(Timer.class), timer);
         connect(croupier.getNegative(Network.class), network, new OverlayFilter(overlayId));
-        
+
         nat = create(NatTraversalComp.class, new NatTraversalComp.NatTraversalInit(selfAddress, init.seed));
+        connect(nat.getNegative(Timer.class), timer);
         connect(nat.getNegative(Network.class), network);
         connect(nat.getNegative(CroupierPort.class), croupier.getPositive(CroupierPort.class));
-        
+
         swim = create(SwimComp.class, new SwimComp.SwimInit(selfAddress, init.bootstrapNodes, init.aggregatorAddress));
         connect(swim.getNegative(Timer.class), timer);
         connect(swim.getNegative(Network.class), nat.getPositive(Network.class));
     }
-    
+
     private Handler<Start> handleStart = new Handler<Start>() {
 
         @Override
